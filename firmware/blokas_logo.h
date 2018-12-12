2b82d5f4d50c03312fc8212f156f56c9ef23f235
Giedrius <giedrius@blokas.io>
2018-12-12 15:15:15 +0200

    Updated bootloader display init and logo

--- firmware/blokas_logo.h
+++ firmware/blokas_logo.h
@@ -3,8 +3,8 @@
 
 #include <avr/pgmspace.h>
 
-enum { BLOKAS_LOGO_WIDTH = 44 };
-enum { BLOKAS_LOGO_HEIGHT = 40 };
+enum { BLOKAS_LOGO_WIDTH = 32 };
+enum { BLOKAS_LOGO_HEIGHT = 32 };
 
 extern const PROGMEM uint8_t BLOKAS_LOGO[BLOKAS_LOGO_WIDTH*BLOKAS_LOGO_HEIGHT/8];
 
--- firmware/midimon.c
+++ firmware/midimon.c
@@ -15,11 +15,11 @@
 #define DDR(p) CONCAT(DDR, p)
 #define PORT(p) CONCAT(PORT, p)
 
-enum { SPI_SS        = (1 << 2) };
-enum { SPI_MOSI      = (1 << 3) };
-enum { SPI_SCK       = (1 << 5) };
-enum { LCD_RST       = (1 << 6) };
-enum { LCD_DC        = (1 << 7) };
+enum { SPI_SS   = (1 << 2) };
+enum { SPI_MOSI = (1 << 3) };
+enum { SPI_SCK  = (1 << 5) };
+enum { LCD_RST  = (1 << 6) };
+enum { LCD_DC   = (1 << 7) };
 
 static void spi_send(uint8_t b)
 {
@@ -52,52 +52,52 @@ static void draw_logo(void)
 {
 	spi_begin();
 
-	uint8_t x = 42;
+	uint8_t x = 48;
 	uint8_t i;
 	uint8_t j;
 	uint8_t y;
 
-	for (j=0; j<1; ++j)
+	for (j = 0; j < 1; ++j)
 	{
 		PORT(LCD_PORT) &= ~LCD_DC;
-		spi_send(0x00);
+		spi_send(0x02);
 		spi_send(0x10);
 		spi_send(0xb0 | j);
 		PORT(LCD_PORT) |= LCD_DC;
-		for (i = 0; i<132; ++i)
+		for (i = 0; i < 128; ++i)
 		{
 			spi_send(0x00);
 		}
 	}
 
-	for (y=0; y<BLOKAS_LOGO_HEIGHT/8; ++j, ++y)
+	for (y = 0; y < BLOKAS_LOGO_HEIGHT / 8; ++j, ++y)
 	{
 		PORT(LCD_PORT) &= ~LCD_DC;
-		spi_send(0x00);
+		spi_send(0x02);
 		spi_send(0x10);
 		spi_send(0xb0 | (j & 0x07));
 		PORT(LCD_PORT) |= LCD_DC;
-		for (i=0; i<x; ++i)
+		for (i = 0; i < x; ++i)
 		{
 			spi_send(0x00);
 		}
-		for (i=0; i<BLOKAS_LOGO_WIDTH; ++i)
+		for (i = 0; i < BLOKAS_LOGO_WIDTH; ++i)
 		{
-			spi_send(~pgm_read_byte(&BLOKAS_LOGO[y*BLOKAS_LOGO_WIDTH+i]));
+			spi_send(~pgm_read_byte(&BLOKAS_LOGO[y*BLOKAS_LOGO_WIDTH + i]));
 		}
-		for (i=x+ BLOKAS_LOGO_WIDTH; i<132; ++i)
+		for (i = x + BLOKAS_LOGO_WIDTH; i < 128; ++i)
 		{
 			spi_send(0x00);
 		}
 	}
-	for (; j<=8; ++j)
+	for (; j < 8; ++j)
 	{
 		PORT(LCD_PORT) &= ~LCD_DC;
-		spi_send(0x00);
+		spi_send(0x02);
 		spi_send(0x10);
 		spi_send(0xb0 | j);
 		PORT(LCD_PORT) |= LCD_DC;
-		for (i = 0; i<132; ++i)
+		for (i = 0; i < 128; ++i)
 		{
 			spi_send(0x00);
 		}
@@ -110,42 +110,61 @@ void midimon_init(void)
 {
 	DDR(SPI_PORT) |= SPI_SS | SPI_MOSI | SPI_SCK;
 	DDR(LCD_PORT) |= LCD_RST | LCD_DC;
+	PORT(LCD_PORT) &= ~LCD_RST;
 	SPCR = (1 << SPE) | (1 << MSTR);
 	SPSR = (1 << SPI2X);
+	_delay_us(15);
 	PORT(LCD_PORT) |= LCD_RST;
-	PORT(LCD_PORT) &= ~LCD_DC; // Command mode.
+
+	PORT(LCD_PORT) &= ~LCD_DC;
+
 	spi_begin();
+	spi_send(0xae); // Disable display.
+	spi_send(0xc8); // Mirror vertically.
+	spi_send(0xa1); // Mirror horizontally.
 
-	spi_send(0x40);
-	spi_send(0xb0);
-	spi_send(0x00); spi_send(0x10);
-	spi_send(0xa0);
-	spi_send(0xc8);
-	spi_send(0xa6);
-	spi_send(0xa2);
-	spi_send(0x2f);
-	spi_send(0xf8); spi_send(0x00);
-	spi_send(0x23);
-	spi_send(0x81); spi_send(0x27);
-	spi_send(0xac);
-	spi_send(0xfa); spi_send(0x83);
-
-	spi_send(0xa4);
-	spi_send(0xaf);
-
+	// Contrast
+	spi_send(0x81);
+	spi_send(0xff);
 	spi_end();
 
+	set_scroll(64 - 8);
+
+#if 0
+	spi_begin();
+	int i, j;
+	for (j = 0; j < 8; ++j)
+	{
+		PORT(LCD_PORT) &= ~LCD_DC;
+
+		spi_send(0xb0 | j);
+		spi_send(0x10);
+		spi_send(0x02);
+
+		PORT(LCD_PORT) |= LCD_DC;
+		for (i = 0; i < 132 * 64; ++i)
+		{
+			spi_send(0x00);
+		}
+	}
+	spi_end();
+#endif
+
 	draw_logo();
-	set_scroll(64 - 4);
+
+	PORT(LCD_PORT) &= ~LCD_DC; // Command mode.
+	spi_begin();
+	spi_send(0xaf); // Enable display.
+	spi_end();
 }
 
 void midimon_uninit(void)
 {
-	DDR(SPI_PORT)  &= ~(SPI_SS | SPI_MOSI | SPI_SCK);
+	DDR(SPI_PORT) &= ~(SPI_SS | SPI_MOSI | SPI_SCK);
 	PORT(SPI_PORT) &= ~(SPI_SS | SPI_MOSI | SPI_SCK);
 
 	// LCD_RST left at HIGH state intentionally.
-	DDR(LCD_PORT)  &= ~(/*LCD_RST |*/ LCD_DC);
+	DDR(LCD_PORT) &= ~(/*LCD_RST |*/ LCD_DC);
 	PORT(LCD_PORT) &= ~(/*LCD_RST |*/ LCD_DC);
 }
 
@@ -156,7 +175,7 @@ void midimon_progress(void)
 
 void midimon_progress_reset(void)
 {
-	while (g_scroll != 60)
+	while (g_scroll != 56)
 	{
 		midimon_progress();
 		_delay_ms(5);
